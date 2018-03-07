ec7cc85029c0cb9fa37ed948b120c4c8dc257514
熊世波 <shibo.xiong@funi.com>
2018-03-07 15:27:09 +0800

    删掉不使用的

--- src/view/main/Main.tsx
+++ src/view/main/Main.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { ScrollView, View } from 'react-native';
 import { SearchBar, Grid } from 'antd-mobile';
-import TopMiddle from './TopMiddle'
-
-const data = Array.from(new Array(10)).map((_val, i) => ({
+const data1 = Array.from(new Array(2)).map((_val, i) => ({
+    icon: 'https://os.alipayobjects.com/rmsportal/IptWdCkrtkAUfjE.png',
+    text: `bar${i}`,
+    url: `u${i}`,
+}));
+const data = Array.from(new Array(30)).map((_val, i) => ({
     icon: 'https://os.alipayobjects.com/rmsportal/IptWdCkrtkAUfjE.png',
     text: `Name${i}`,
+    url: `u${i}`,
 }));
+
 
 export default class Main extends React.Component<any, any> {
     render() {
@@ -18,8 +23,8 @@ export default class Main extends React.Component<any, any> {
                         showCancelButton
                     />
                 </View>
-                <Grid data={data} columnNum={4} hasLine={false} isCarousel onClick={(_el, index) => alert(index)} />
-                <TopMiddle />
+                <Grid data={data1} columnNum={1} carouselMaxRow={1} isCarousel onClick={(_el: any, index) => alert(_el.url)} />
+                <Grid data={data} columnNum={5} isCarousel onClick={(_el: any, index) => alert(_el.url)} />
             </ScrollView>
         );
     }
